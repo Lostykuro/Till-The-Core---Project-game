ae23f45a5069a610b8bb683ad1d0868c2df32b35
João Araujo <juniormedeirosales@gmail.com>
2025-05-16 15:45:39 -0300

    Basic feature implementation
    -added player animations/actions
    -added stone degrading logic
    -changed stone sprites
    -fixed minor logic errors

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,119 @@
 extends CharacterBody2D
 
 const SPEED := 100
-const HIT_COOLDOWN := 1.0   # segundos de espera entre hits
+var current_dir := "down"  # direção padrão ao iniciar
+var current_action:= "none"
+var is_moving := false
+var on_action := false
 
-var current_dir := "none"
-var hit_timer := 0.0
+var hit_cooldown := 0.5  # em segundos
+var time_since_last_hit := 0.0
+var use_cooldown := 0.8  # em segundos
+var time_since_last_use := 0.0
+
+@onready var sprite := $AnimatedSprite2D
+@onready var hitbox_front := $Hitbox/Front_hitbox
+@onready var hitbox_back := $Hitbox/Back_hitbox
+
+var already_hit_targets := []
+
+func _ready():
+	sprite.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
+
 
 func _physics_process(delta):
-	# — atualiza cooldown
-	if hit_timer > 0.0:
-		hit_timer = max(hit_timer - delta, 0.0)
-
-	# — se estiver atacando, não se move
-	var anim = $AnimatedSprite2D
-	if anim.animation == "hit" and anim.is_playing():
-		velocity = Vector2.ZERO
-		move_and_slide()
-		return
-
-	# — movimento
+	time_since_last_hit += delta
+	time_since_last_use += delta
 	var input_vec := Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right";  input_vec.x += 1
-	if Input.is_action_pressed("ui_left"):
-		current_dir = "left";   input_vec.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		current_dir = "down";   input_vec.y += 1
-	if Input.is_action_pressed("ui_up"):
-		current_dir = "up";     input_vec.y -= 1
+	is_moving = false
+	if !on_action:
+		if Input.is_action_pressed("Move_right"):
+			current_dir = "right";  input_vec.x += 1
+		if Input.is_action_pressed("Move_left"):
+			current_dir = "left";   input_vec.x -= 1
+		if Input.is_action_pressed("Move_down"):
+			current_dir = "down";   input_vec.y += 1
+		if Input.is_action_pressed("Move_up"):
+			current_dir = "up";     input_vec.y -= 1
+ 
+	if Input.is_action_pressed("Hit") and not on_action and time_since_last_hit >= hit_cooldown:
+		on_action = true
+		current_action = "hit"
 
+	if Input.is_action_pressed("Use") and not on_action and time_since_last_use>=use_cooldown:
+		on_action = true; current_action="use"
+	
+	
 	if input_vec != Vector2.ZERO:
+		is_moving = true
 		input_vec = input_vec.normalized()
 		velocity = input_vec * SPEED
 	else:
 		velocity = Vector2.ZERO
 
 	move_and_slide()
+	play_animation()
+
+func play_animation():
+	if is_moving:
+		if current_dir == "right":
+			sprite.flip_h = false
+			sprite.play("walk_side")
+		elif current_dir == "left":
+			sprite.flip_h = true
+			sprite.play("walk_side")
+		elif current_dir == "up":
+			sprite.flip_h = false
+			sprite.play("walk_back")
+		elif current_dir =="down":
+			sprite.flip_h = false
+			sprite.play("walk_front")
+
+	elif !is_moving and !on_action:
+		if current_dir == "down" or current_dir == "left" or current_dir == "right":
+			sprite.play("Idle_front")
+		elif current_dir == "up":
+			sprite.play("Idle_back")
+			
+	elif on_action:
+		if current_action== "hit" and (current_dir == "down" or current_dir == "left" or current_dir == "right"):
+			sprite.play("hit_front") 
+		elif current_action== "hit" and current_dir == "up":
+			sprite.play("hit_back")
+		elif current_action=="use":
+			current_dir=="down"
+			sprite.play("use")
+		
+			
+func _on_AnimatedSprite2D_animation_finished():
+	if on_action:
+		if current_action== "hit":
+			print("just hitted")
+			time_since_last_hit = 0.0 
+			apply_hit()
+			on_action = false
+			current_action = "none"
+		if current_action== "use":
+			print("just used")
+			time_since_last_use=0.0
+			on_action = false
+			current_action = "none"
+		else:
+			on_action=false
+			current_action = "none"
+
+
+func apply_hit():
+	var hitbox = hitbox_back if current_dir == "up" else hitbox_front
+	var bodies = hitbox.get_overlapping_bodies()
 	
+	for body in bodies:
+		if body is Node2D and body.has_method("take_damage"):
+			if body in already_hit_targets:
+				continue  # já atingido nesse hit
+				print("Atingido:", body.name)
+			body.take_damage(1)
+			already_hit_targets.append(body)
+
+	# Limpa a lista pra próxima vez
+	already_hit_targets.clear()
---
+++ scripts/stone.gd
@@ -0,0 +1,51 @@
+extends Node2D
+@export_range(0, 100) var Initial_hit_points: int = 4
+@export_range(0, 100) var hit_points: int = 4
+@export var is_destroyed: bool = false
+@export_range(0, 3) var destruction_level: int = 0
+
+@onready var sprite := $AnimatedSprite2D
+
+
+
+func _ready():
+	hit_points=Initial_hit_points
+	if is_destroyed:
+		hit_points=0
+		$CollisionShape2D.disabled = true
+		destruction_level=3
+	update_sprite()
+
+func set_destruction_level(value):
+	if is_destroyed:
+		return  # impede alterações
+	destruction_level = clamp(value, 0, 3)
+	update_sprite()
+	
+	if destruction_level == 3:
+		is_destroyed = true
+		$CollisionShape2D.disabled = true
+		z_index = -1  # pedra atrás do player
+		
+func take_damage(amount: int):
+	if is_destroyed:
+		return
+	hit_points -= amount
+	print("Dano recebido:", amount, "| HP restante:", hit_points)
+	if hit_points <= 0:
+		set_destruction_level(3)
+	elif hit_points <= (Initial_hit_points / 3):
+		set_destruction_level(2)
+	elif hit_points <= (Initial_hit_points / 2):
+		set_destruction_level(1)
+
+func update_sprite():
+	match destruction_level:
+		0:
+			sprite.frame = 0  # estágio intacto
+		1:
+			sprite.frame = 1  # meio destruída
+		2:
+			sprite.frame = 2  # só o restinho
+		3:
+			sprite.frame = 3 # completamente destruída
